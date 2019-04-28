ded1a840b7c2aeca02c9f5d6c53129f73aaddb3e
SenpaixDev <37963948+SenpaixDev@users.noreply.github.com>
2019-04-28 13:55:49 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = commands.Bot(command_prefix='/')
 async def greet(ctx):
     await ctx.send(":smiley: :wave: Hello, there!")
 
-bot.run('NTcxNzM3MjI1MzIzMzQ3OTY4.XMTgGg.Yi1e2oCmiuAEjiwgIlAXbrq5ThY')
+bot.run('NTcxNzM3MjI1MzIzMzQ3OTY4.XMTgGg.Yi1e2oCmiuAEjiwgIlAXbrq5ThY') 
 
 
 
