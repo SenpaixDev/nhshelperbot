f3260b2f7a9a11beca76e3908a13187bbaab959f
SenpaixDev <37963948+SenpaixDev@users.noreply.github.com>
2019-04-28 13:54:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,10 @@ bot = commands.Bot(command_prefix='$')
 
 @bot.event
 async def on_ready():
-    print ("0 Errors found so far.")
-    print ("NHS Helper Bot is online")
-    await bot.change_presence(game=discord.Game(name='Do "/cmds" to get started!'))
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
 bot.run('NTcxNzM3MjI1MzIzMzQ3OTY4.XMTgGg.Yi1e2oCmiuAEjiwgIlAXbrq5ThY')
 
